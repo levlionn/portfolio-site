4367f0cfb2058c56e5e8138e9c8e469788948495
Lev Markelov <lev@snakebyte.ca>
2023-02-01 17:29:14 -0500

    demo

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,32 +1,13 @@
+import ExpertizeSection from "./ExpertizeSection";
 import HeroSection from "./heroSection";
 import ProjectSection from "./projectSection";
-import PortfolioCard from "@/components/PortfolioCard";
-
-// Images
-import firstPic from "../../public/005.jpg";
 
 export default function Home() {
   return (
     <main className="h-screen">
       <HeroSection />
       <ProjectSection />
-      <PortfolioCard
-        source={firstPic}
-        altText="Food Coma portfolio image"
-        client="Food Coma"
-        cardHeader="Social Media Campaign Strategy"
-        cardBody="Drove 202 email signups in 6 days."
-        tags={["#socialmedia", "#advertising"]}
-      />
-
-      <PortfolioCard
-        source={firstPic}
-        altText="Call Center Guys portfolio image"
-        client="Call Center Guys"
-        cardHeader="Website Revamp"
-        cardBody="Designed, developed, deployed a brand new website. Followed by running marketing operations and creative for a call center company based in Canada."
-        tags={["#uxui", "#websitecopy"]}
-      />
+      <ExpertizeSection />
     </main>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import ToggleActiveMode from "./ToggleActiveMode";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between py-10 items-center  sticky top-0 z-50 bg-white">
+    <div className="flex justify-between py-10 items-center sticky top-0 z-50 bg-white">
       <div className="flex items-center">
         <div className="space-y-2">
           <span className="block w-8 h-0.5 bg-gray-600"></span>
