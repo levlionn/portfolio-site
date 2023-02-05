168c4ec01130b87a9031338e204892e2a8bc03f1
Lev Markelov <lev@snakebyte.ca>
2023-02-05 16:42:38 -0500

    --no-amend

--- components/ContactMeButton.tsx
+++ components/ContactMeButton.tsx
@@ -10,8 +10,8 @@ const ContactMeButton = ({ colour }: Props) => {
     <div className="max-w-s md:max-w-2xl my-10 p-6 space-y-3 bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden flex flex-col items-center ">
       <h2 className="text-lg font-bold text-center">Got a project?</h2>
       <p className="text-center font-medium">
-        I&apos;d love to hear from you! Drop me a message and let's bring it to
-        life.
+        I&apos;d love to hear from you! Drop me a message and let&apos;s bring
+        it to life.
       </p>
       <CoolButton
         btnLabel="Get in touch!"
