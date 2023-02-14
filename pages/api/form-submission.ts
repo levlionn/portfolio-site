01f7a82645d24bf2bebb4ee454831c5081bfa4db
Lev Markelov <lev@snakebyte.ca>
2023-02-14 14:33:23 -0500

    hooked up sendgrid

--- pages/api/form-submission.ts
+++ pages/api/form-submission.ts
@@ -3,18 +3,21 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { FormData, HCaptchaObject } from "@/app/contact/ContactForm";
 import { verify } from "hcaptcha";
 import { HCAPTCHA_SECRET } from "@/constants";
+import sgMail from "@sendgrid/mail";
 
 const MAX_FIELD_LENGTH = 10000;
 const TO_ADDRESS = "lev@levm.me";
-const FROM_ADDRESS = "send@sendgrid.com";
+const FROM_ADDRESS = "lev@levm.me";
 
 type ResponseData = { success: true } | { success: false; errMsg: string };
 
 type VerifiedFormData = FormData & HCaptchaObject;
 
-const send = (data: unknown) => {
-  console.log(data);
-};
+// const send = (data: unknown) => {
+//   console.log(data);
+// };
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY ?? "");
 
 export default async function handler(
   req: NextApiRequest,
@@ -59,13 +62,12 @@ export default async function handler(
   const truncatedToken = token.substring(0, MAX_FIELD_LENGTH);
   const truncatedEkey = ekey.substring(0, MAX_FIELD_LENGTH);
 
-  console.log("right before trycat for human verifaction.");
   try {
     const hCaptchaData = await verify(HCAPTCHA_SECRET ?? "", truncatedToken);
     //this is a bait&switch.
     //if a person is trying to spam my site, and they don't pass human verification, it will 'appear' like the did, but lol, they didn't.
     if (!hCaptchaData.success) {
-      console.log("Somebody was a bot but successful.");
+      console.log("Somebody was a bot but we fake a success status.");
       res.status(200).json({ success: true });
       return;
     }
@@ -79,10 +81,10 @@ export default async function handler(
   }
 
   try {
-    await send({
+    await sgMail.send({
       to: TO_ADDRESS,
       from: FROM_ADDRESS,
-      subject: "Contact Form Requeset [levm]",
+      subject: "[levm] 👋 Someone Contacted You!",
       html: `
       <p>[Name]: ${truncatedName}</p>
       <p>[Email]: ${truncatedEmail}</p>
